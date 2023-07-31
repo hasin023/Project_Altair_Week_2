fd08fe06ed10dda54f8eac8b02dcac70ee3863d6
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-31 23:11:33 +0600

    Detecting red and white colors

--- Python Programming and OpenCV/Part 2/openCV.py
+++ Python Programming and OpenCV/Part 2/openCV.py
@@ -1,12 +1,25 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('img/GOAT.jpg', 0)
-img = cv2.resize(img, (0, 0), fx=0.6, fy=0.6)
-img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+img = cv2.imread('img/GOAT.jpg', 1)
+img = cv2.resize(img, (0, 0), fx=0.8, fy=0.8)
 
-cv2.imwrite('img/GOAT2.jpg', img)
 
-cv2.imshow("Image", img)
+hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+
+red_lower = np.array([136, 87, 111], np.uint8)
+red_upper = np.array([180, 255, 255], np.uint8)
+red_mask = cv2.inRange(hsvImg, red_lower, red_upper)
+res_red = cv2.bitwise_and(img, img, mask=red_mask)
+cv2.imshow("Red", res_red)
+
+
+white_lower = np.array([0, 0, 200], np.uint8)
+white_upper = np.array([180, 30, 255], np.uint8)
+white_mask = cv2.inRange(hsvImg, white_lower, white_upper)
+res_white = cv2.bitwise_and(img, img, mask=white_mask)
+cv2.imshow("White", res_white)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
