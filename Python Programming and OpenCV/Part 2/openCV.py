819363939be6b918345b750482110c3c93444eb1
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-03 21:16:11 +0600

    Fixed openCV methods

--- Python Programming and OpenCV/Part 2/openCV.py
+++ Python Programming and OpenCV/Part 2/openCV.py
@@ -1,54 +1,63 @@
 import numpy as np
 import cv2
 
-# Ditecting Red and White Color with Masks
-img = cv2.imread('img/GOAT.jpg', -1)
-smallImg = cv2.resize(img, (0, 0), fx=0.8, fy=0.8)
-cv2.imshow("Original-Small", smallImg)
-
-# TODO NEED TO MAKE THIS A SEPARATE DETECT_COLOR METHOD
-hsvImg = cv2.cvtColor(smallImg, cv2.COLOR_BGR2HSV)
+from PIL import Image
 
 
-red_lower = np.array([136, 87, 111], dtype=np.uint8)
-red_upper = np.array([178, 255, 255], dtype=np.uint8)
-red_mask = cv2.inRange(hsvImg, red_lower, red_upper)
-res_red = cv2.bitwise_and(smallImg, smallImg, mask=red_mask)
-cv2.imshow("Red", res_red)
+def detect_red_and_white_regions(image):
+    img = cv2.imread(image, -1)
+    smallImg = cv2.resize(img, (0, 0), fx=0.8, fy=0.8)
+    cv2.imshow("Original-Small", smallImg)
+
+    hsvImg = cv2.cvtColor(smallImg, cv2.COLOR_BGR2HSV)
+
+    red_lower = np.array([136, 87, 111], dtype=np.uint8)
+    red_upper = np.array([178, 255, 255], dtype=np.uint8)
+    red_mask = cv2.inRange(hsvImg, red_lower, red_upper)
+    res_red = cv2.bitwise_and(smallImg, smallImg, mask=red_mask)
+    cv2.imshow("Red", res_red)
+
+    white_lower = np.array([0, 0, 200], dtype=np.uint8)
+    white_upper = np.array([180, 30, 255], dtype=np.uint8)
+    white_mask = cv2.inRange(hsvImg, white_lower, white_upper)
+    res_white = cv2.bitwise_and(smallImg, smallImg, mask=white_mask)
+    cv2.imshow("White", res_white)
+
+    white_red = cv2.addWeighted(res_red, 1.0, res_white, 1.0, 0.0)
+    cv2.imshow("White_Red", white_red)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-white_lower = np.array([0, 0, 200], dtype=np.uint8)
-white_upper = np.array([180, 30, 255], dtype=np.uint8)
-white_mask = cv2.inRange(hsvImg, white_lower, white_upper)
-res_white = cv2.bitwise_and(smallImg, smallImg, mask=white_mask)
-cv2.imshow("White", res_white)
+def analyze_goat(image_array):
+    # Print Minimum and maximum pixel values in the image
+    max_value = np.max(image_array)
+    min_value = np.min(image_array)
+    print("Maximum pixel value: ", max_value)
+    print("Minimum pixel value: ", min_value)
+
+    # Print Average pixel value in the image
+    average_pixel = int(np.mean(image_array))
+    print("Average_pixel: ", average_pixel)
+
+    # Print Total number of non-zero pixels in the image
+    shape = image_array.shape
+    print("Shape of the image: ", shape)
+
+    non_zero_pixels = np.count_nonzero(image_array)
+    print("Total number of non-zero pixels: ", non_zero_pixels)
+
+    # Print Total number of zero pixels in the image
+    zero_pixels = np.prod(shape) - non_zero_pixels
+    print("Total number of zero pixels: ", zero_pixels)
 
 
-white_red = cv2.addWeighted(res_red, 1.0, res_white, 1.0, 0.0)
-cv2.imshow("White_Red", white_red)
+if __name__ == '__main__':
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    goat = "img/Goat.jpg"
+    detect_red_and_white_regions(goat)
 
-
-# TODO NEED TO MAKE THIS A SEPARATE ANALYZE_GOAT METHOD
-# Print Minimum and maximum pixel values in the image
-max_value = np.max(img)
-min_value = np.min(img)
-print("Maximum pixel value: ", max_value)
-print("Minimum pixel value: ", min_value)
-
-# Print Average pixel value in the image
-average_pixel = int(np.mean(img))
-print("Average_pixel: ", average_pixel)
-
-# Print Total number of non-zero pixels in the image
-shape = img.shape
-print("Shape of the image: ", shape)
-
-non_zero_pixels = np.count_nonzero(img)
-print("Total number of non-zero pixels: ", non_zero_pixels)
-
-# Print Total number of zero pixels in the image
-zero_pixels = np.prod(shape) - non_zero_pixels
-print("Total number of zero pixels: ", zero_pixels)
+    img = Image.open("img/Goat.jpg")
+    img_data = np.asarray(img)
+    analyze_goat(img_data)
--- ROS ( Robot Operating System )/turtle_controller.py
+++ ROS ( Robot Operating System )/turtle_controller.py
@@ -5,6 +5,18 @@ import rospy
 from math import radians
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+from turtlesim.srv import SetPen
+
+previous_x = 0
+
+
+def call_set_pen_service(r, g, b, width, off):
+    rospy.wait_for_service("/turtle1/set_pen")
+    try:
+        set_pen = rospy.ServiceProxy("/turtle1/set_pen", SetPen)
+        set_pen(r, g, b, width, off)
+    except rospy.ServiceException as e:
+        rospy.logerr(e)
 
 
 def process_instructions(instructions):
@@ -24,18 +36,21 @@ def process_instructions(instructions):
 
     for i in instructions:
         if i == 'F':
+            # call_set_pen_service(255, 255, 255, 5, 0)
             rospy.logwarn("Moving forward")
             publisher.publish(move_cmd)
             rate.sleep()
 
         elif i == 'L':
             for x in range(0, 10):
+                call_set_pen_service(255, 0, 0, 5, 0)
                 rospy.logwarn("Turning Left")
                 publisher.publish(turn_L_cmd)
                 rate.sleep()
 
         elif i == 'R':
             for x in range(0, 10):
+                call_set_pen_service(0, 255, 0, 5, 0)
                 rospy.logwarn("Turning Right")
                 publisher.publish(turn_R_cmd)
                 rate.sleep()
@@ -54,6 +69,15 @@ def avoid_border(data):
         avoid_cmd.angular.z = 0.0
 
     publisher.publish(avoid_cmd)
+
+    global previous_x
+    if data.x >= 5.5 and previous_x < 5.5:
+        rospy.logwarn("Crossed the middle line")
+        call_set_pen_service(0, 255, 0, 5, 0)
+    elif data.x <= 5.5 and previous_x > 5.5:
+        rospy.logwarn("Crossed the middle line")
+        call_set_pen_service(255, 0, 0, 5, 0)
+    previous_x = data.x
 
 
 def draw_square():
